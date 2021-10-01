f842ceff3e9563af85d63f6526e822c552dfa6a7
Luqman Hakim <hakim.13luqman@gmail.com>
2021-10-01 09:53:07 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		System.out.println("Nama : Luqman Hakim");
+		System.out.println("NIM : 6706210067");
+		System.out.println("Kelas : D3RPLA-45-02");
+	}
+}
